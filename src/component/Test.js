f1b465e6903e6aeea32a37760a0c996b081a0fe9
maedemotlagh <maede.mo15@gmail.com>
2022-03-15 16:43:44 +0330

    add navbar html

--- src/component/Test.js
+++ src/component/Test.js
@@ -1,10 +1,46 @@
 import React from "react"
+import './style.scss'
 
+const MenuItem = [
+    {
+        name:'Resources',
+        id:'1',
+    },
+    {
+        name:'Pricing',
+        id:'2'
+    },
+    {
+        name:'Features',
+        id:'3'
+    },
+    {
+        name:'Business Types',
+        id:'4'
+    },
+    {
+        name:'EnterPrise',
+        id:'5',
+    }
+]
 const Test = () => {
 
     return (
-        <div className="Test">
-            uu
+        <div className="Header">
+          <div className="Navbar">
+            <div className="LoginItem">
+                <div className="LogIn">LogIn</div>
+                <div className="Demo">Demo & Pricing</div>
+            </div>
+            <div className="MenuItem">
+                {
+                    MenuItem.map(item => <p>{item.name}</p>)
+                }
+            </div>
+            <div className="Logo">
+                <img src="/img/logome.svg"/>
+            </div>
+          </div>
         </div>
     )
 }
